cba2c5a61785e62cfa4bf4c319bc769b18fef5ac
ben <ben@ben-1000.(none)>
2011-02-16 19:54:25 -0600

    r1

--- cs 552/hw2_2/adder16.v
+++ cs 552/hw2_2/adder16.v
@@ -1,20 +1,6 @@
 module adder16(A, B, Cin, S, Cout);
    input [15:0] A, B;
    input 	Cin;
-<<<<<<< HEAD
-   output 	Cout;
-   output [15:0] S;
-   wire [3:0] 	 G, P;
-   wire [2:0] 	 C;
- 	
-   fourbitfull f1(A[3:0], B[3:0], Cin, S[3:0], G[0], P[0], C[0]);
-   fourbitfull f2(A[7:4], B[7:4], C[0], S[7:4], G[1], P[1], C[1]);
-   fourbitfull f3(A[11:8], B[11:8], C[1], S[11:8], G[2], P[2], C[2]);
-   fourbitfull f4(A[15:12], B[15:12], C[2], S[15:12], G[3], P[3], Cout);
-endmodule // adder16
-
-   
-=======
    output [15:0] S;
    output 	 Cout;
    wire [3:0] 	 G, P;
@@ -39,5 +25,4 @@ endmodule // adder16
    adder4 a3(A[11:8], B[11:8], C[1], S[11:8]);
    adder4 a4(A[15:12], B[15:12], C[2], S[15:12]);
 endmodule
- 
->>>>>>> 4522979e66c4c4928486f4586e006d20a0267740
+
