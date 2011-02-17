65e16b3d1463accebae6fe2f686453644d7be5ac
ben <ben@ben-1000.(none)>
2011-02-16 19:56:55 -0600

    Revert "Revert "signed tests""
    This reverts commit c6ace7195cd579a2d8afc4fa5272dd362d7fa4a2.

--- cs 552/hw2_2/alu.v
+++ cs 552/hw2_2/alu.v
@@ -11,31 +11,36 @@ module alu (A, B, Cin, Op, invA, invB, sign, Out, Ofl, Z);
    output 	 Z;
    wire [15:0] 	 Ai, Bi, O1, AddOut;
    reg [15:0] 	 tmp;
-   wire 	 AddCout, SignedOfl;
- 	 
+   reg 		 tmpOfl;
+   wire 	 AddCout, SignedOfl, Bit15Cin;
    
    assign Ai = invA ? ~A : A;
    assign Bi = invB ? ~B : B;
    shifter s1(.In(Ai), .Cnt(Bi[3:0]), .Op(Op[1:0]), .Out(O1));
    adder16 a1(.A(Ai), .B(Bi), .Cin(Cin), .S (AddOut), .Cout (AddCout));
    // In signed addition, overflow is determined by
-   // whether the signs of the input and the output
-   // is the same
-   assign SignedOfl = 
+   // whether the carry in to the last bit is the same as the carry
+   // out. We can deduce the carry-in by whether the value changed.
+   assign Bit15Cin = AddOut[15] ? ~(A[15] ^ B[15]) : (A[15] ^ B[15]);
+   assign SignedOfl = (Bit15Cin == AddCout);
+   
+   
    
    always @* case (Op)
-	3'b0__: begin tmp = O1; Ofl = 0; end
+	3'b0__: begin tmp = O1; tmpOfl = 0; end
 	3'b100: begin 
 	   tmp = AddOut;
-	   Ofl = AddCout;
+	   tmpOfl = sign ? SignedOfl : AddCout;
 	end
-	3'b101: begin tmp = Ai | Bi; Ofl = 0; end
-	3'b110: begin tmp = Ai ^ Bi; Ofl = 0; end
-	3'b111: begin tmp = Ai & Bi; Ofl = 0; end
+	3'b101: begin tmp = Ai | Bi; tmpOfl = 0; end
+	3'b110: begin tmp = Ai ^ Bi; tmpOfl = 0; end
+	3'b111: begin tmp = Ai & Bi; tmpOfl = 0; end
 	default: $display("Error: incorrect Op in ALU");
    endcase // case (Op)
    
    assign Out = tmp;
+   assign Ofl = tmpOfl;
+   
    // It's zero if none of the bits are 1
    assign Z = ~(|Out);
    
