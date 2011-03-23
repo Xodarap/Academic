8e7574f03b3fabd66aa34e3985f5a0d1a5c09478
ben <ben@ben-1000.(none)>
2011-03-22 23:14:14 -0500

    hook up a few more things

--- cs 552/project/fetch.v
+++ cs 552/project/fetch.v
@@ -5,12 +5,11 @@ module fetch(Clk, Rst, PcSrc, NewPc, Instruction);
    output [15:0]	Instruction;
    
    wire [15:0] pc;           // PC
-   wire [15:0] instruction;  // Current Instruction
    wire [15:0] foo;          // Instruction to write
    wire [15:0] pcPlusTwo;    // PC + 2
    wire [15:0] nextPc;   
    
-   memory2c instruction_memory(.data_out(instruction), 
+   memory2c instruction_memory(.data_out(Instruction), 
 			       .data_in(foo),          // We never write instructions
 			       .addr(pc), 
 			       .enable(1), 
