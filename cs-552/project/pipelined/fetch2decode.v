9b2ff86ee3380c253390853c9a154bf2c5780271
Benjamin West <west@mumble-21.cs.wisc.edu>
2011-04-11 21:39:22 -0500

    better dir structure

--- cs-552/project/pipelined/fetch2decode.v
+++ cs-552/project/pipelined/fetch2decode.v
@@ -1,20 +1,34 @@
 module fetch2decode(Clk, Rst,
 		    RegDestIn, Reg1In, Reg2In, Reg3In,
-		    Stall, Instruction,
+		    Stall, Instruction, pcIn, immIn, pcplustwoin,
 		    /* Out */		    
-		    RegDestOut, Reg1Out, Reg2Out, Reg3Out
+		    RegDestOut, Reg1Out, Reg2Out, Reg3Out,
+			pcOut, immOut, pcplustwoout
 		    );
 
    input Clk, Rst, Stall;
    input [1:0] RegDestIn;
    input [2:0] Reg1In, Reg2In, Reg3In;
    input [15:0] Instruction;
+   input [15:0] pcIn;
+   input [15:0] immIn;
+   input [15:0] pcplustwoin;
+   output [15:0] pcplustwoout;
+   output [15:0] immOut;
+   output [15:0] pcOut;
    output [1:0] RegDestOut;
    output [2:0] Reg1Out, Reg2Out, Reg3Out;
    
    wire [4:0] 	foo;
    wire [15:0] 	nextInstruction;   
+
+
+   reg16 pcpls(.writedata(pcplustwoin), .readdata(pcplustwoout), .write(~Stall), .clk(Clk), .rst(Rst));
    
+   reg16 pcreg(.writedata(pcIn), .readdata(pcOut), .write(~Stall), .clk(Clk), .rst(Rst));
+   
+   reg16 immreg(.writedata(immIn), .readdata(immOut), .write(~Stall), .clk(Clk), .rst(Rst));
+
    reg16 controlSignals(
 			/* We only use the 12 lowest bits */
 			.writedata({5'b0, RegDestIn, Reg1In, Reg2In, Reg3In}), 
--- cs-552/project/pipelined/hazarddetector.v
+++ cs-552/project/pipelined/hazarddetector.v
@@ -1,9 +1,12 @@
-module hazarddetector(XMemRead, XRt, DRs, DRt, Stall);
-   input XMemRead;
-   input [2:0] XRt, DRs, DRt;
+module hazarddetector(XMemRead, XRt, DRs, DRt, MRd, XRd,MRegWrite, XRegWrite, RType, Stall);
+   input XMemRead, MRegWrite, XRegWrite, RType;
+   input [2:0] XRt, DRs, DRt, MRd, XRd;
    output Stall;
-
-   assign Stall = 1'b0;
-//XMemRead & (XRt == DRs | XRt == DRt);
+   wire   fmem, fex;
+   
+   assign fex = XRegWrite & (XRd == DRs | (XRd == DRt && RType));
+   assign fmem= MRegWrite & (MRd == DRs | (MRd == DRt && RType));
+   
+   assign Stall = (XMemRead & (XRt == DRs | (XRt == DRt && RType))) | fex | fmem;
    
 endmodule // hazarddetector
