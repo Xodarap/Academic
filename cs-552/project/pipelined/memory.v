77698117e42a6c2c48e81487b7c9e2ad88e8e97f
ben <ben@ben-1000.(none)>
2011-04-26 18:36:50 -0500

    stall mem start

--- cs-552/project/pipelined/memory.v
+++ cs-552/project/pipelined/memory.v
@@ -1,10 +1,12 @@
-module memory(Clk, Rst, Addr, Data, MemWrite, MemRead, ReadData);
+module memory(Clk, Rst, Addr, Data, MemWrite, MemRead, ReadData, Stall);
    input Clk, Rst;
    input [15:0] Addr, Data;
    input 	MemWrite, MemRead;
    output [15:0] ReadData;
+   output 	 Stall;
    wire [15:0] 	 readTemp;
-   
+   wire 	 done, cacheHit;
+   /*
    
    memory2c memory0(.data_out(readTemp), 
 		    .data_in(Data), 
@@ -13,6 +15,22 @@ module memory(Clk, Rst, Addr, Data, MemWrite, MemRead, ReadData);
 		    .wr(MemWrite), 
 		    .createdump(1'b0), 
 		    .clk(Clk), 
-		    .rst(Rst));
+		    .rst(Rst));*/
+   stallmem memory0(/*AUTOINST*/
+		    // Outputs
+		    .DataOut		(readTemp),
+		    .Done		(done),
+		    .Stall		(Stall),
+		    .CacheHit		(CacheHit),
+		    .err		(err),
+		    // Inputs
+		    .DataIn		(DataIn[15:0]),
+		    .Addr		(Addr[15:0]),
+		    .Wr			(MemWrite),
+		    .Rd			(MemRead),
+		    .createdump		(1'b0),
+		    .clk		(Clk),
+		    .rst		(Rst));
+   
    assign ReadData = readTemp & {16{MemRead}};
 endmodule // memory
--- cs-552/project/pipelined/memory2writeback.v
+++ cs-552/project/pipelined/memory2writeback.v
@@ -1,8 +1,8 @@
 module memory2writeback(Clk, Rst, 
 			RegWriteIn, ReadDataIn, ALUResultIn, DestRegIn, MemToRegIn, InstructionIn,
-			RegWriteOut, ReadDataOut, ALUResultOut, DestRegOut, MemToRegOut, InstructionOut);
+			RegWriteOut, ReadDataOut, ALUResultOut, DestRegOut, MemToRegOut, InstructionOut, Stall);
 
-	input Clk, Rst, RegWriteIn, MemToRegIn;
+	input Clk, Rst, RegWriteIn, MemToRegIn, Stall;
 	input [15:0] ReadDataIn, ALUResultIn, InstructionIn;
 	input [2:0] DestRegIn;
    
@@ -13,10 +13,10 @@ module memory2writeback(Clk, Rst,
    wire [10:0] 	     garbage;
    
 	
-	reg16 readData(.clk(Clk), .rst(Rst), .write(1'b1), .readdata(ReadDataOut), .writedata(ReadDataIn));
-	reg16 ALUData(.clk(Clk), .rst(Rst), .write(1'b1), .readdata(ALUResultOut), .writedata(ALUResultIn));
-	reg16 instruction(.clk(Clk), .rst(Rst), .write(1'b1), .readdata(InstructionOut), .writedata(InstructionIn));
-	reg16 writeAndDest(.clk(Clk), .rst(Rst), .write(1'b1), 
+	reg16 readData(.clk(Clk), .rst(Rst), .write(Stall), .readdata(ReadDataOut), .writedata(ReadDataIn));
+	reg16 ALUData(.clk(Clk), .rst(Rst), .write(Stall), .readdata(ALUResultOut), .writedata(ALUResultIn));
+	reg16 instruction(.clk(Clk), .rst(Rst), .write(Stall), .readdata(InstructionOut), .writedata(InstructionIn));
+	reg16 writeAndDest(.clk(Clk), .rst(Rst), .write(Stall), 
 				.readdata({garbage, MemToRegOut, DestRegOut, RegWriteOut}),
 				.writedata({11'b0, MemToRegIn, DestRegIn, RegWriteIn}));
 
